4263ff93b45378353ae78ac62497389a342aace6
Maria Bogdanova <masha.bogdanoff@gmail.com>
2023-10-30 20:51:15 +0100

    fix: remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,7 @@ import ErrorBoundary from './components/error-boundary/ErrorBoundary';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <ErrorBoundary>
       <App />
     </ErrorBoundary>
-  </React.StrictMode>
 );

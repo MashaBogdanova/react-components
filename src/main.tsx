97d87240b9a4684e790dac8499937bb0f4da5db8
Maria Bogdanova <masha.bogdanoff@gmail.com>
2023-10-30 22:27:40 +0100

    fix: scripts

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import ErrorBoundary from './components/error-boundary/ErrorBoundary';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
 );

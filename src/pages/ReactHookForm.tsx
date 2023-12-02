4d9ddd4a98060b9c778b41fd52d9c7fe24091f8f
Maria Bogdanova <masha.bogdanoff@gmail.com>
2023-12-02 12:50:09 +0100

    feat: add uncontrolled form

--- src/pages/ReactHookForm.tsx
+++ src/pages/ReactHookForm.tsx
@@ -1,4 +1,5 @@
 import { useForm, SubmitHandler } from 'react-hook-form';
+import styles from './Forms.module.css';
 
 type Inputs = {
   name: string;
@@ -8,7 +9,7 @@ type Inputs = {
   repeatPassword: string;
   gender: boolean;
   agreement: boolean;
-  picture: any;
+  avatar: any;
 };
 
 export default function ReactHookForm() {
@@ -20,97 +21,114 @@ export default function ReactHookForm() {
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
-  console.log(watch('picture')); // watch input value by passing the name of it
+  console.log(watch('avatar')); // watch input value by passing the name of it
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        {...register('name', {
-          pattern: {
-            value: /^[A-Z].*$/,
-            message:
-              'Name should begins with a capital letter. Please enter a valid name accordingly.',
-          },
-        })}
-      />
-      {errors.name && errors.name.message}
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+      <fieldset className={styles.form__fieldset}>
+        <label>Your name</label>
+        <input
+          {...register('name', {
+            pattern: {
+              value: /^[A-Z].*$/,
+              message:
+                'Name should begins with a capital letter. Please enter a valid name accordingly.',
+            },
+          })}
+        />
+        {errors.name && errors.name.message}
 
-      <input
-        type="number"
-        {...register('age', {
-          valueAsNumber: true,
-          validate: {
-            positive: (v) =>
-              v > 0 ||
-              'Age should be greater than 0. Please enter a valid age accordingly',
-            integer: (v) =>
-              Number.isInteger(v) ||
-              'Age should be an integer. Please enter a valid age accordingly',
-          },
-        })}
-      />
-      {errors.age && errors.age.message}
+        <label>Your age</label>
+        <input
+          type="number"
+          {...register('age', {
+            valueAsNumber: true,
+            validate: {
+              positive: (v) =>
+                v > 0 ||
+                'Age should be greater than 0. Please enter a valid age accordingly',
+              integer: (v) =>
+                Number.isInteger(v) ||
+                'Age should be an integer. Please enter a valid age accordingly',
+            },
+          })}
+        />
+        {errors.age && errors.age.message}
+      </fieldset>
 
-      <input
-        {...register('email', {
-          pattern: {
-            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-            message:
-              'Please enter a valid email address. Ensure it follows the standard format, such as user@example.com.',
-          },
-        })}
-      />
-      {errors.email && errors.email.message}
+      <fieldset className={styles.form__fieldset}>
+        <label>E-mail</label>
+        <input
+          {...register('email', {
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message:
+                'Please enter a valid email address. Ensure it follows the standard format, such as user@example.com.',
+            },
+          })}
+        />
+        {errors.email && errors.email.message}
+      </fieldset>
 
-      <input
-        {...register('password', {
-          pattern: {
-            value:
-              /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()_+])[0-9A-Za-z!@#$%^&*()_+]+$/,
-            message:
-              'Please enter a valid Password. Ensure it contains 1 number, 1 uppercased letter, 1 lowercased letter, 1 special character.',
-          },
-        })}
-      />
-      {errors.password && errors.password.message}
+      <fieldset className={styles.form__fieldset}>
+        <label>Password</label>
+        <input
+          {...register('password', {
+            pattern: {
+              value:
+                /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()_+])[0-9A-Za-z!@#$%^&*()_+]+$/,
+              message:
+                'Please enter a valid Password. Ensure it contains 1 number, 1 uppercased letter, 1 lowercased letter, 1 special character.',
+            },
+          })}
+        />
+        {errors.password && errors.password.message}
 
-      <input
-        {...register('repeatPassword', {
-          pattern: {
-            value:
-              /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()_+])[0-9A-Za-z!@#$%^&*()_+]+$/,
-            message:
-              'Please enter a valid Password. Ensure it contains 1 number, 1 uppercased letter, 1 lowercased letter, 1 special character.',
-          },
-          validate: (v) =>
-            v === watch('password') ||
-            'Passwords do not match. Please ensure that the entered passwords match exactly.',
-        })}
-      />
-      {errors.repeatPassword && errors.repeatPassword.message}
-
-      <input type="radio" {...register('gender')} />
-      <input type="radio" {...register('gender')} />
-
-      <input type="checkbox" {...register('agreement', { required: true })} />
-      {errors.agreement && <span>This field is required.</span>}
-
-      <input
-        type="file"
-        {...register('picture', {
-          validate: {
-            extension: (v) =>
-              v[0].type === 'image/jpeg' ||
-              v[0].type === 'image/png' ||
-              'Please upload an image in JPEG or PNG format.',
-            size: (v) =>
-              v[0].size <= 1024 * 1024 ||
-              'Please make sure the image size is within the limit of 1MB.',
-          },
-        })}
-      />
-      {/*{errors.picture && errors.picture.message}*/}
-
+        <label>Repeat password</label>
+        <input
+          {...register('repeatPassword', {
+            pattern: {
+              value:
+                /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()_+])[0-9A-Za-z!@#$%^&*()_+]+$/,
+              message:
+                'Please enter a valid Password. Ensure it contains 1 number, 1 uppercased letter, 1 lowercased letter, 1 special character.',
+            },
+            validate: (v) =>
+              v === watch('password') ||
+              'Passwords do not match. Please ensure that the entered passwords match exactly.',
+          })}
+        />
+        {errors.repeatPassword && errors.repeatPassword.message}
+      </fieldset>
+      <fieldset className={styles.form__fieldset}>
+        <label>Female</label>
+        <input type="radio" {...register('gender')} checked />
+        <label>Male</label>
+        <input type="radio" {...register('gender')} />
+      </fieldset>
+      <fieldset className={styles.form__fieldset}>
+        <label>Your avatar</label>
+        <input
+          type="file"
+          {...register('avatar', {
+            validate: {
+              extension: (v) =>
+                v[0].type === 'image/jpeg' ||
+                v[0].type === 'image/png' ||
+                'Please upload an image in JPEG or PNG format.',
+              size: (v) =>
+                v[0].size <= 1024 * 1024 ||
+                'Please make sure the image size is within the limit of 1MB.',
+            },
+          })}
+        />
+        {/*{errors.avatar && errors.avatar.message}*/}
+      </fieldset>
+      <fieldset className={styles.form__fieldset}>
+        <label>I agree with terms and conditions:</label>
+        <input type="checkbox" {...register('agreement', { required: true })} />
+        {errors.agreement && <span>This field is required.</span>}
+      </fieldset>
       <input type="submit" />
     </form>
   );
--- src/pages/UncontrolledForm.tsx
+++ src/pages/UncontrolledForm.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import styles from './Forms.module.css';
 
 function UncontrolledForm() {
-  return <div></div>;
+  const handleSubmit = () => {};
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <fieldset className={styles.form__fieldset}>
+        <label htmlFor="name">Your name</label>
+        <input type="text" id="name" />
+        <label htmlFor="age">Your age</label>
+        <input type="number" id="age" />
+      </fieldset>
+      <fieldset className={styles.form__fieldset}>
+        <label htmlFor="email">E-mail</label>
+        <input type="email" id="email" />
+      </fieldset>
+      <fieldset className={styles.form__fieldset}>
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" />
+        <label htmlFor="repeatPassword">Repeat password</label>
+        <input type="password" id="repeatPassword" />
+      </fieldset>
+      <fieldset className={styles.form__fieldset}>
+        <label htmlFor="female">Female</label>
+        <input type="radio" id="female" name="gender" checked/>
+        <label htmlFor="male">Male</label>
+        <input type="radio" id="male" name="gender" />
+      </fieldset>
+      <fieldset className={styles.form__fieldset}>
+        <label htmlFor="avatar">Your avatar</label>
+        <input type="file" id="avatar" />
+      </fieldset>
+      <fieldset className={styles.form__fieldset}>
+        <input type="checkbox" id="agreement" />
+        <label htmlFor="agreement">I agree with terms and conditions</label>
+      </fieldset>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default UncontrolledForm;
